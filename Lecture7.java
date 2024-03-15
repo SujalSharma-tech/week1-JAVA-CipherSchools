2d1a099f3eba37798a886be4005f4c4017ef5654
Sujal <sharmasujal995@gmail.com>
2024-03-15 17:39:50 +0530

    till lecture 7

---
+++ Lecture7.java
@@ -0,0 +1,8 @@
+public class Lecture7 {
+    // Heap & Stack- Class 7
+    public static void main(String[] args) {
+        int x = 5;
+        Integer x1 = new Integer(x);
+        System.out.println(x1);
+    }
+}
